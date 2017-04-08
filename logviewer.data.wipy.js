a38ed05fb82cf2d9111e7ef9c44f26937cc83882
Dave Smith <dave.g.smith@gmail.com>
2017-04-08 15:58:11 +0100

    Attempt at better filtering of noisy data from Wipy

--- logviewer.data.wipy.js
+++ logviewer.data.wipy.js
@@ -4,12 +4,14 @@ function processWiPyLog(series,data) {
 	var maxValues={}
 
 	var headers=data[0].split(',')
+	var prevValue=[]
 	for(var i = 0;i<headers.length;i++) {
 		headers[i]=headers[i].trim()
-		
+
 		series[headers[i]]=[];
+		prevValue[i]=2000
 	}
-	
+
 	for (var i = 1 ; i < data.length;i++) {
 		var values=data[i].split(',')
 		for (var j=0;j<values.length;j++) {
@@ -19,17 +21,22 @@ function processWiPyLog(series,data) {
 				var numMillis=(dataPoint*1000)+946684800000
 				dataPoint=new Date(numMillis)
 			}
-			else if(dataPoint > 20000) {
-				dataPoint=null
+			else{
+			    var diff = Math.abs(dataPoint-prevValue[j]);
+			    var roc = diff/prevValue[j];
+
+			    if(dataPoint > 20000 || roc > 5) {
+				    dataPoint=prevValue[j]
+			    }
 			}
-				
+            prevValue[j]=dataPoint
 			series[seriesName].push(dataPoint)
 			manageMaxMin(dataPoint,seriesName,maxValues,minValues)
-
 		}
 	}
+
 	series.XAxis="Time"
-	series.defaultSelections=["28ffcf7572150223","28ffeb73531502f2"]
+	series.defaultSelections=["28ff00cb721502ea","28ffeb73531502f2"]
 	series.headers=headers
 	series.maxValues=maxValues
 	series.minValues=minValues
@@ -37,3 +44,5 @@ function processWiPyLog(series,data) {
 	return series
 
 }
+
+
